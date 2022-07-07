557e37eb9b5020f98984994febbcc5d3f5e8e44e
Jeo <jeo961@gmail.com>
2022-07-07 13:53:19 -0400

    Created mather class

---
+++ PairingDemo/Mather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairingDemo
+{
+    internal class Mather
+    {
+        //take in 2 doubles, add then return them
+        public double adder(double x, double y)
+        {
+            return x + y;
+        }
+        //take in 2 doubles, subract then return
+        public double subber(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
